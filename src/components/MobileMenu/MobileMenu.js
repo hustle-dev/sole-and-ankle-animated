9dffd2faff869a3815f9f97817dfd304a9a17820
hustle-dev <dlwoabsdk@gmail.com>
2023-04-23 12:35:57 +0900

    feat: Exercise 3

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from 'react'
+import styled, { keyframes } from 'styled-components/macro'
+import { DialogOverlay, DialogContent } from '@reach/dialog'
 
-import { QUERIES, WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants'
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from '../UnstyledButton'
+import Icon from '../Icon'
+import VisuallyHidden from '../VisuallyHidden'
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   return (
@@ -33,8 +33,26 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
         </Footer>
       </Content>
     </Overlay>
-  );
-};
+  )
+}
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
+
+const slideIn = keyframes`
+from {
+  transform: translateX(100%);
+}
+to {
+  transform: translateX(0);
+}
+`
 
 const Overlay = styled(DialogOverlay)`
   position: fixed;
@@ -45,7 +63,9 @@ const Overlay = styled(DialogOverlay)`
   background: var(--color-backdrop);
   display: flex;
   justify-content: flex-end;
-`;
+
+  animation: ${fadeIn} 500ms ease-out;
+`
 
 const Content = styled(DialogContent)`
   background: white;
@@ -54,20 +74,27 @@ const Content = styled(DialogContent)`
   padding: 24px 32px;
   display: flex;
   flex-direction: column;
-`;
+  z-index: 100;
+
+  animation: ${slideIn} 400ms ease-in-out;
+`
 
 const CloseButton = styled(UnstyledButton)`
   position: absolute;
   top: 10px;
   right: 0;
   padding: 16px;
-`;
+
+  animation: ${fadeIn} 300ms ease-out 300ms backwards;
+`
 
 const Nav = styled.nav`
   display: flex;
   flex-direction: column;
   gap: 16px;
-`;
+
+  animation: ${fadeIn} 300ms ease-out 300ms backwards;
+`
 
 const NavLink = styled.a`
   color: var(--color-gray-900);
@@ -79,23 +106,25 @@ const NavLink = styled.a`
   &:first-of-type {
     color: var(--color-secondary);
   }
-`;
+`
 
 const Filler = styled.div`
   flex: 1;
-`;
+`
 const Footer = styled.footer`
   flex: 1;
   display: flex;
   flex-direction: column;
   gap: 14px;
   justify-content: flex-end;
-`;
+
+  animation: ${fadeIn} 300ms ease-out 300ms backwards;
+`
 
 const SubLink = styled.a`
   color: var(--color-gray-700);
   font-size: 0.875rem;
   text-decoration: none;
-`;
+`
 
-export default MobileMenu;
+export default MobileMenu
